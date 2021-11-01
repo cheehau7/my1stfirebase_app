f6d586203ab5f8a01a926cbd51520d4da27cd828
Chee Hau <hewcheehau@Hews-MacBook-Pro.local>
2021-11-01 16:39:45 +0800

    Movie class with riverpod

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+final mySystemTheme = SystemUiOverlayStyle.dark;
+final myDarkSystemTheme = SystemUiOverlayStyle.dark;
+
